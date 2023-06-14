91325a60127c7235ae6dfbb0f3ddf962175c527b
sebastientromp <sebastien.tromp@gmail.com>
2023-06-14 13:02:23 +0200

    update dev dependencies

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,25 +1,24 @@
 module.exports = {
 	parser: '@typescript-eslint/parser',
 	parserOptions: {
-		ecmaVersion: 2018, // Allows for the parsing of modern ECMAScript features
+		ecmaVersion: 2022,
 		sourceType: 'module',
-		project: './tsconfig.json',
 	},
-	plugins: ['@typescript-eslint/eslint-plugin'],
+	plugins: ['@typescript-eslint', 'prettier'],
+	env: {
+		node: true,
+		es2022: true,
+	},
 	extends: [
+		'eslint:recommended',
 		'plugin:@typescript-eslint/recommended', // Uses the recommended rules from the @typescript-eslint/eslint-plugin
-		'plugin:prettier/recommended', // Enables eslint-plugin-prettier and displays prettier errors as ESLint errors. Make sure this is always the last configuration in the extends array.
-		'eslint-config-prettier', // Uses eslint-config-prettier to disable ESLint rules from @typescript-eslint/eslint-plugin that would conflict with prettier
+		'prettier',
 	],
 	rules: {
 		'no-multi-spaces': 2,
-		'prefer-rest-params': 0,
-		'no-unused-vars': 0,
-		'@typescript-eslint/explicit-module-boundary-types': 0,
 		'@typescript-eslint/explicit-function-return-type': 0, // Temporary, until code violations are fixed
 		'@typescript-eslint/no-explicit-any': 0, // Temporary, until code violations are fixed
 		'@typescript-eslint/no-parameter-properties': 0,
-		'@typescript-eslint/ban-ts-ignore': 0,
 		'@typescript-eslint/explicit-member-accessibility': [
 			2,
 			{
@@ -40,25 +39,14 @@ module.exports = {
 				'allow-arguments': true,
 			},
 		],
-		// '@typescript-eslint/no-unused-vars': 0,
 		'@typescript-eslint/no-unused-vars': [
-			'warn',
+			1,
 			{
-				'vars': 'all',
-				'args': 'none', // When implementing interfaces, I find it's easier to read if all parameters are always declared
-				'ignoreRestSiblings': true, // For object destructuring
+				vars: 'all',
+				args: 'none', // When implementing interfaces, I find it's easier to read if all parameters are always declared
+				ignoreRestSiblings: false,
 			},
 		],
 		'@typescript-eslint/no-use-before-define': 0,
-		'@typescript-eslint/ban-ts-comment': 'off',
-		'@angular-eslint/no-input-rename': 'off',
-		'@angular-eslint/no-output-on-prefix': 'off',
-		'linebreak-style': 0,
-		'prettier/prettier': [
-			'error',
-			{
-				'endOfLine': 'auto',
-			},
-		],
 	},
 };
