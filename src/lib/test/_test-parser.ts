dd46cdfeacb07656904dbd140ba8156974b21e6b
sebastientromp <sebastien.tromp@gmail.com>
2023-07-30 14:38:18 +0200

    fix quest difficulty parsing in BG

--- src/lib/test/_test-parser.ts
+++ src/lib/test/_test-parser.ts
@@ -1,6 +1,6 @@
 import { AllCardsService } from '@firestone-hs/reference-data';
-import { parseBattlegroundsGame, parseHsReplayString } from '../xml-parser';
-import { xml } from './bg-game.xml';
+import { parseHsReplayString } from '../xml-parser';
+import { xml } from './bgs-quests.xml';
 
 const test = async () => {
 	const allCards = new AllCardsService();
@@ -8,8 +8,8 @@ const test = async () => {
 	const replay = parseHsReplayString(xml, allCards);
 	const mainPlayerCardId = replay.mainPlayerCardId;
 	console.debug('mainPlayerCardId', mainPlayerCardId);
-	const parsed = parseBattlegroundsGame(xml, null, [], [], allCards);
-	console.debug('parsed', parsed.hpOverTurn[mainPlayerCardId]);
+	// const parsed = parseBattlegroundsGame(xml, null, [], [], allCards);
+	console.debug('parsed', replay.bgsHeroQuests);
 	// const cardsPlayedParser = new ActivePlayerCardsPlayedParser(replay, allCards);
 	// parseGame(replay, [cardsPlayedParser]);
 	// console.debug('cards played by turn', cardsPlayedParser.entitiesPlayedPerTurn.toJS());
